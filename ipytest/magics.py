e0926061526f3c6e464e053b501932392cae18fc
CMP <c.prohm@googlemail.com>
2018-10-28 21:10:35 +0100

    Remove deprecation warning for ipytest.magics import

--- ipytest/magics.py
+++ ipytest/magics.py
@@ -1,8 +1,3 @@
 from ._config import config
-from ._util import emit_deprecation_warning
-
-emit_deprecation_warning(
-    "Importing the magics module is deprecated, use 'ipytest.config.magics = True'."
-)
 
 config.magics = True
