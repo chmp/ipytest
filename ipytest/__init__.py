f7c424d12a68f1126fe5d98020b25e42dd3bd1ac
CMP <c.prohm@googlemail.com>
2018-10-28 11:38:55 +0100

    Add base argument as config

--- ipytest/__init__.py
+++ ipytest/__init__.py
@@ -21,8 +21,13 @@ else:
     _has_pytest = True
 
 
-__all__ = (["run_pytest"] if _has_pytest else []) + [
+# the pytest exit code
+exit_code = None
+
+
+__all__ = (["run_pytest", "run"] if _has_pytest else []) + [
     "config",
+    "exit_code",
     "run_tests",
     "clean_tests",
     "collect_tests",
@@ -30,5 +35,4 @@ __all__ = (["run_pytest"] if _has_pytest else []) + [
     "get_assert_function",
     "unittest_assert_equals",
     "reload",
-    "run",
 ]
