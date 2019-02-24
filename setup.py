e733f719ef7b7f23dc1ac9f405183b6bf5fb19b5
CMP <c.prohm@googlemail.com>
2019-02-24 19:32:38 +0100

    Bump version and update version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def _read(fname):
 
 setup(
     name="ipytest",
-    version="0.5.0b4",
+    version="0.5.0b5",
     description="Unit tests in IPython notebooks.",
     long_description=_read("Readme.md"),
     author="Christopher Prohm",
