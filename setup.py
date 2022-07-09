b3ddfa0e567633960cb15383210fe29d757a2d1d
Christopher Prohm <mail@cprohm.de>
2022-07-09 15:28:29 +0200

    pre-relase

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="ipytest",
-    version="0.13.0a1",
+    version="0.13.0a2",
     description="Unit tests in IPython notebooks.",
     long_description=pathlib.Path("Readme.md").read_text(),
     long_description_content_type="text/markdown",
